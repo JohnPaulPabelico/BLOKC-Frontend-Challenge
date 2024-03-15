a0383a83a12b70ca1d1ae7dfd84f398fe52c4664
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-03-15 12:41:53 +0800

    added ipfs protocol

--- dapp/next.config.mjs
+++ dapp/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ipfs.infura.io",
+        port: "",
+        pathname: "/ipfs/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
