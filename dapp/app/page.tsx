2737792fd14a645ca00b1298ef3374f8dcffbb70
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-03-15 12:42:50 +0800

    BASTA MARAMI DAGDAG

--- dapp/app/page.tsx
+++ dapp/app/page.tsx
@@ -1,112 +1,204 @@
+"use client";
+import { useEffect, useState } from "react";
+import { BrowserProvider } from "ethers";
 import Image from "next/image";
+import Background from "../public/images/BG.jpg";
+import { fetchWithRetry } from "../components/fetchWithRetry";
 
 export default function Home() {
+  const [walletKey, setwalletKey] = useState("");
+  const [nativeBalance, setNativeBalance] = useState("0");
+  const [nftCount, setNftCount] = useState(0);
+  const [nftNames, setNftNames] = useState<
+    { name: string; description: string; image: string }[]
+  >([]);
+
+  const balanceString = nativeBalance?.toString();
+
+  const getBalance = async () => {
+    if (!walletKey) {
+      window.alert("Wallet key is empty. Please enter a valid wallet key.");
+      return;
+    }
+
+    try {
+      const balance = await fetchWithRetry(
+        `https://blokc-backend-challenge.onrender.com/balance/${walletKey}`
+      );
+      if (!balance.ok) {
+        throw new Error("Failed to fetch balance");
+      }
+
+      const dataBalance = await balance.json();
+
+      const roundedBalance = parseFloat(
+        dataBalance.balanceInEth.balanceInEth
+      ).toFixed(4);
+
+      setNativeBalance(roundedBalance);
+      console.log(nativeBalance);
+    } catch (error) {
+      window.alert(
+        "Failed to fetch balance. Please check your wallet address."
+      );
+      console.error("Error fetching balance:", error);
+    }
+  };
+
+  const getNfts = async () => {
+    if (!walletKey) {
+      window.alert("Wallet key is empty. Please enter a valid wallet key.");
+      return;
+    }
+
+    try {
+      const NFTs = await fetchWithRetry(
+        `https://blokc-backend-challenge.onrender.com/nft/${walletKey}`
+      );
+      if (!NFTs.ok) {
+        throw new Error("Failed to fetch NFTs");
+      }
+
+      const responseData = await NFTs.json();
+
+      if (
+        !responseData ||
+        !responseData.nfts ||
+        !responseData.nfts.nfts ||
+        !Array.isArray(responseData.nfts.nfts)
+      ) {
+        throw new Error(
+          "Unexpected response format. Expected an array of NFTs."
+        );
+      }
+      const dataNFTs = responseData.nfts.nfts;
+
+      const nftDetails = dataNFTs.map((nft: any) => {
+        const { name, metadata } = nft;
+        const nftName = metadata?.name || name;
+        const nftDescription = metadata?.description || "";
+        const nftImage = metadata?.image || "";
+        return { name: nftName, description: nftDescription, image: nftImage };
+      });
+
+      setNftCount(dataNFTs.length);
+      setNftNames(nftDetails);
+
+      console.log("NFT Details", nftDetails);
+      console.log("Count", nftDetails.length);
+    } catch (error) {
+      window.alert("Failed to fetch NFTs. Please check your wallet address.");
+      console.error("Error fetching NFTs:", error);
+    }
+  };
+
+  const walletChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    setwalletKey(inputValue);
+    console.log(inputValue);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main
+      className="min-h-screen"
+      style={{
+        backgroundImage: `url(${Background.src})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundPositionY: "80%",
+        overflow: "hidden",
+      }}
+    >
+      <div className="text-white relativ grid text-center lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left bg-black bg-opacity-50">
+        <div className="flex lg:flex justify-center items-center lg:justify-start lg:items-start p-4 ">
+          <Image
+            src="/images/arbitrum.png"
+            alt="Left Image"
+            width={60}
+            height={60}
+            className=""
+          />
+          <div className="">
+            <h1 className="font-sans font-bold text-2xl">Arbitrum</h1>
+            <h1 className="font-sans font-bold text-2xl">Fetcher</h1>
+          </div>
+        </div>
+
+        <div className="lg:ml-auto ml-0 items-center  p-4 lg:mr-10">
+          <input
+            type="text"
+            className="mt-2 border rounded-md p-2 focus:outline-none focus:ring-4 focus:ring-blue-300 focus:border-transparent lg:w-96 w-1/2"
+            value={walletKey}
+            onChange={(e) => walletChange(e)}
+            placeholder="Enter wallet address here"
+            style={{ color: "black" }}
+          />
+        </div>
+
+        <div className="lg:ml-auto ml-0 flex justify-center items-center  p-4 ">
+          <span className="font-sans font-bold text-2xl">
+            Balance: {balanceString}&nbsp;
+          </span>
+          <p className="font-sans text-white text-2xl"></p>
+          <button
+            onClick={() => {
+              getBalance();
+            }}
           >
-            By{" "}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/images/refresh.svg"
+              alt="Left Image"
+              width={20}
+              height={20}
+              className="ml-4 mt-1"
+              style={{ filter: "invert(1)", transition: "transform 0.3s" }}
+              onMouseEnter={(e) => {
+                e.currentTarget.style.transform = "scale(1.1)";
+              }}
+              onMouseLeave={(e) => {
+                e.currentTarget.style.transform = "scale(1)";
+              }}
             />
-          </a>
+          </button>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex justify-center items-center">
+        <button
+          onClick={() => {
+            getNfts();
+          }}
+          className="mt-4 text-white font-sans font-bold text-2xl rounded-lg p-4 bg-black bg-opacity-30 transition duration-200 ease-in-out hover:bg-opacity-50 hover:shadow-lg "
+        >
+          Fetch NFTs
+        </button>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className="mr-10 ml-10 ">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 mt-4">
+          {nftNames.map((nft, index) => (
+            <div key={index} className="bg-white rounded-lg shadow-md p-4">
+              <div className="flex justify-center">
+                <Image
+                  src={`https://ipfs.infura.io/ipfs/${
+                    nft.image.split("://")[1]
+                  }`}
+                  alt={nft.name}
+                  width={200}
+                  height={200}
+                  className="object-cover rounded-lg"
+                />
+              </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+              {/* NFT Details */}
+              <div className="mt-4">
+                <h3 className="text-lg text-black font-semibold">{nft.name}</h3>
+                <p className="mt-2 text-gray-600">{nft.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   );
